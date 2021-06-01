85b92c0c4169812fd04a0fe7f75c1a753081809c
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-01 20:23:47 +0530

    calculated the distance bw 2 points (length of a line)

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,23 @@
 package linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Line Comparison Computation program");
+		int x1,x2,y1,y2;
+		double distance;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the point x1 ");
+		x1 = scan.nextInt();
+		System.out.println("Enter the point y1 ");
+		y1 = scan.nextInt();
+		System.out.println("Enter the point x2 ");
+		x2 = scan.nextInt();
+		System.out.println("Enter the point y2 ");
+		y2 = scan.nextInt();
+		
+		distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("Length of a line is " + distance);
 	}
 }
