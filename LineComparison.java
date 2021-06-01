2b4a46ddd91238f03c16cb429af357e713a6ed42
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-01 21:34:29 +0530

    Checked equality of 2 lines

--- LineComparison.java
+++ LineComparison.java
@@ -2,11 +2,12 @@ package linecomparison;
 
 import java.util.Scanner;
 
+
 public class LineComparison {
 	public static void main(String[] args) {
-		int x1,x2,y1,y2;
-		double distance;
-		Scanner scan = new Scanner(System.in);
+		int x1,x2,y1,y2,a1,a2,b1,b2;
+		Double distance1, distance2;
+		Scanner scan= new Scanner(System.in);
 		
 		System.out.println("Enter the point x1 ");
 		x1 = scan.nextInt();
@@ -16,8 +17,28 @@ public class LineComparison {
 		x2 = scan.nextInt();
 		System.out.println("Enter the point y2 ");
 		y2 = scan.nextInt();
+		System.out.println("Enter the point a1 ");
+		a1 = scan.nextInt();
+		System.out.println("Enter the point b1 ");
+		b1 = scan.nextInt();
+		System.out.println("Enter the point a2 ");
+		a2 = scan.nextInt();
+		System.out.println("Enter the point b2 ");
+		b2 = scan.nextInt();
 		
-		distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println("Length of a line is " + distance);
+		distance1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		distance2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+
+		System.out.println("Length of a line1 is " + distance1);
+		System.out.println("Length of a line2 is " + distance2);
+	
+		if(distance1.equals(distance2))
+		{
+			System.out.println("equal");
+		}
+			else { 
+				System.out.println("not equal");
+			}
+		
 	}
 }
